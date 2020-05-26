b407db9db5a19f7a1482dc0008d38cb27570b19e
srh <1072230030@qq.com>
2020-05-26 20:24:12 +0800

    用户列表功能

--- src/main.js
+++ src/main.js
@@ -9,10 +9,18 @@ import router from './router'
 import './plugins/element.js'
 // 引入全局样式表
 import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
 
 // 配置请求的根路径
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须return config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
