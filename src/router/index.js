3199c9ce75da97d1aeb82409148fee7acf9a69fd
srh <1072230030@qq.com>
2020-06-09 09:17:58 +0800

    完成了权限功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome'
-import Users from '../components/User/Users'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/User/Users.vue'
+import Rights from '../components/Power/Rights.vue'
+import Roles from '../components/Power/Roles.vue'
 
 
 Vue.use(VueRouter)
@@ -17,9 +19,12 @@ const router = new VueRouter({
       component: Home, 
       redirect: '/welcome',
       children: [
-        {path: '/welcome', component: Welcome},
-        {path: '/users', component: Users},]
-      },
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users },
+        { path: '/rights', component: Rights },
+        { path: '/roles', component: Roles }
+      ]
+    },
    ]
 });
 
